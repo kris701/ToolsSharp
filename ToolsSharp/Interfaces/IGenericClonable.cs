ab480cadd78627f4dfb8bd4410aad7c867ef551f
Kristian Skov <kris701kj@gmail.com>
2024-12-10 08:02:00 +0100

    Added a "empty" serialization model and comments

--- ToolsSharp/Interfaces/IGenericClonable.cs
+++ ToolsSharp/Interfaces/IGenericClonable.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace ToolsSharp.Interfaces
 {
+	/// <summary>
+	/// A version of <seealso cref="ICloneable"/> where the output type is known at compile time
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
 	public interface IGenericClonable<T>
 	{
+		/// <summary>
+		/// Clone the given object
+		/// </summary>
+		/// <returns>A clones instance of the current object of type <typeparamref name="T"/></returns>
 		public T Clone();
 	}
 }
---
+++ ToolsSharp/Serialization/EmptyModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolsSharp.Serialization
+{
+	/// <summary>
+	/// An empty model that can be used for JSON or XML serialization/deserialization
+	/// </summary>
+	public class EmptyModel
+	{
+	}
+}
