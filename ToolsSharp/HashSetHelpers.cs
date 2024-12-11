91615843be7d3edd49a34d2a51831b4dd24fc7d5
Kristian Skov <kris701kj@gmail.com>
2024-12-11 08:24:41 +0100

    Option to range remove items from a hashset

--- ToolsSharp/HashSetHelpers.cs
+++ ToolsSharp/HashSetHelpers.cs
@@ -11,10 +11,22 @@
         /// <typeparam name="T"></typeparam>
         /// <param name="set"></param>
         /// <param name="other"></param>
-        public static void AddRange<T>(this HashSet<T> set, HashSet<T> other)
+        public static void AddRange<T>(this HashSet<T> set, IEnumerable<T> other)
         {
             foreach (var item in other)
                 set.Add(item);
         }
-    }
+
+		/// <summary>
+		/// Remove all the values from <paramref name="other"/> into <paramref name="set"/>.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="set"></param>
+		/// <param name="other"></param>
+		public static void RemoveRange<T>(this HashSet<T> set, IEnumerable<T> other)
+		{
+			foreach (var item in other)
+				set.Remove(item);
+		}
+	}
 }
