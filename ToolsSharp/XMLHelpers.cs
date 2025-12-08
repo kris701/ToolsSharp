e5987a67306d29ee24a706e4ee767c1f47db41d9
Kristian Skov <kris701kj@gmail.com>
2025-12-08 11:50:39 +0100

    Added a XML helper

---
+++ ToolsSharp/XMLHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ToolsSharp
+{
+	public static class XMLHelpers
+	{
+		public static XElement? GetElementByPath(this XElement parent, string path, string nameSpace = "")
+		{
+			XNamespace ns = nameSpace;
+
+			var split = path.Split('.', StringSplitOptions.RemoveEmptyEntries);
+			var current = parent;
+			foreach (var item in split)
+			{
+				current = current.Element(ns + item);
+				if (current == null)
+					return null;
+			}
+			return current;
+		}
+	}
+}
