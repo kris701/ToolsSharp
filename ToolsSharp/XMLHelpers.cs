0d01a5114cf9d7e142df59a8e7a35e63b513e0f8
Kristian Skov <kris701kj@gmail.com>
2025-12-12 08:05:39 +0100

    Added more XML helpers

--- ToolsSharp/XMLHelpers.cs
+++ ToolsSharp/XMLHelpers.cs
@@ -1,9 +1,12 @@
-﻿using System.Xml.Linq;
+﻿using System.Globalization;
+using System.Xml.Linq;
 
 namespace ToolsSharp
 {
 	public static class XMLHelpers
 	{
+		public static XElement? GetElementByPath(this XElement parent, string path, XNamespace nameSpace) => parent.GetElementByPath(path, nameSpace.NamespaceName);
+
 		public static XElement? GetElementByPath(this XElement parent, string path, string nameSpace = "")
 		{
 			XNamespace ns = nameSpace;
@@ -17,6 +20,21 @@ namespace ToolsSharp
 					return null;
 			}
 			return current;
+		}
+
+		public static decimal? GetDecimalValue(this XElement xElement)
+		{
+			return decimal.Parse(xElement.Value, new CultureInfo("en-US"));
+		}
+
+		public static int? GetIntValue(this XElement xElement)
+		{
+			return int.Parse(xElement.Value);
+		}
+
+		public static DateTime? GetDateTimeValue(this XElement xElement)
+		{
+			return DateTime.Parse(xElement.Value);
 		}
 	}
 }
