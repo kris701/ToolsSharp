b251a6457e3b9d24bab173a08cf9607453361671
Kristian Skov <kris701kj@gmail.com>
2026-05-26 17:11:22 +0200

    Added a force directory delete method

--- ToolsSharp/DirectoryHelper.cs
+++ ToolsSharp/DirectoryHelper.cs
@@ -17,5 +17,34 @@
 			path = path.Replace("\\", "/");
 			return path;
 		}
+
+		/// <summary>
+		/// Force delete an entire directory
+		/// </summary>
+		/// <param name="path"></param>
+		public static void DeleteDirectory(string path)
+		{
+			var dirInfo = new DirectoryInfo(path);
+			try
+			{
+				SetAttributesNormal(dirInfo);
+				Directory.Delete(path, true);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Error: {e.Message}");
+			}
+		}
+
+		private static void SetAttributesNormal(DirectoryInfo dir)
+		{
+			foreach (var subDir in dir.GetDirectories())
+				SetAttributesNormal(subDir);
+			foreach (var file in dir.GetFiles())
+			{
+				file.Attributes = FileAttributes.Normal;
+			}
+			dir.Attributes = FileAttributes.Normal;
+		}
 	}
 }
