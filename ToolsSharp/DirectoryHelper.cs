b18d8d5f1202608670d359ae427fb50f8c76338d
Kristian Skov <kris701kj@gmail.com>
2026-05-27 07:23:36 +0200

    Added more directory helpers

--- ToolsSharp/DirectoryHelper.cs
+++ ToolsSharp/DirectoryHelper.cs
@@ -46,5 +46,26 @@
 			}
 			dir.Attributes = FileAttributes.Normal;
 		}
+
+		/// <summary>
+		/// Copy all contents of a directory to another directory.
+		/// https://stackoverflow.com/a/3822913
+		/// </summary>
+		/// <param name="sourcePath"></param>
+		/// <param name="targetPath"></param>
+		public static void CopyFilesRecursively(string sourcePath, string targetPath)
+		{
+			//Now Create all of the directories
+			foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+			{
+				Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
+			}
+
+			//Copy all the files & Replaces any files with the same name
+			foreach (string newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
+			{
+				File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
+			}
+		}
 	}
 }
