a7266df2b37f8905ac9a24cd17ea0a6c3137319b
Kristian Skov <kris701kj@gmail.com>
2026-05-27 11:27:43 +0200

    Added mroe XML helper methods

--- ToolsSharp/XMLHelpers.cs
+++ ToolsSharp/XMLHelpers.cs
@@ -41,5 +41,33 @@ namespace ToolsSharp
 		{
 			return DateTime.Parse(xElement.Value);
 		}
+
+		public static string ConvertXDocumentToString(XDocument doc)
+		{
+			if (doc.Root == null)
+				throw new ArgumentNullException("Root element is missing from the XDocument!");
+			return ConvertXElementToString(doc.Root);
+		}
+
+		public static string ConvertXElementToString(XElement element)
+		{
+			string result;
+
+			// Set the declaration for the document to indicate that it's using UTF-8 encoding.
+			if (element.Document != null)
+				element.Document.Declaration = new XDeclaration("1.0", "utf-8", null);
+
+			// Save the XElement to a StringWriter and get the resulting string.
+			using (var writer = new StringWriter())
+			{
+				element.Save(writer);
+				result = writer.ToString();
+			}
+
+			// Replace the declaration, which is set to UTF-16 by default, with one that specifies UTF-8 encoding.
+			result = result.Replace("<?xml version=\"1.0\" encoding=\"utf-16\"?>", "<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+
+			return result;
+		}
 	}
 }
